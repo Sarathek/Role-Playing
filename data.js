cc1e15131ba17ff2585d9c1b8b82de2dd4a2cb15
Sarathek <135970994+Sarathek@users.noreply.github.com>
2023-08-02 12:26:26 +0530

    Progress

---
+++ data.js
@@ -0,0 +1,16 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "assets/wizard.png",
+        health: 60,
+        diceCount: 3
+    },
+    monster: {
+        name: "Orc",
+        avatar: "assets/orc.png",
+        health: 30,
+        diceCount: 1
+    }
+}
+
+export default characterData
